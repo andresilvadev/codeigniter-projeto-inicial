f7e35021d66547339ca19231cf0020222d60966c
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2019-04-19 20:09:26 -0300

    Criando visualização da listagem de dados

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -1,6 +1,32 @@
+
+<table class="table table-hover">
+    <thead>
+    <tr>
+        <th>#</th>
+        <th>Página</th>
+        <th>Slug</th>
+        <th class="text-right">Ações</th>
+    </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($pages as $page) : ?>        
+            <tr>
+                <td><?php echo $page->id ;?></td>
+                <td><?php echo $page->title ;?></td>
+                <td><?php echo $page->slug ;?></td>
+                <td class="text-right">
+                    <a href="/pages/<?php echo $page->id; ?>" class="btn btn-xs btn-default">Ver</a>
+                    <a href="/pages/<?php echo $page->id; ?>/edit" class="btn btn-xs btn-info">Editar</a>
+                    <form action="/pages/<?php echo $page->id; ?>/delete" style="display: inline-block">
+                        <input type="submit" value="remover" class="btn btn-xs btn-danger">
+                    </form>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
 <?php
-
-
 echo '<pre>'; print_r($pages); echo '</pre>';
 
 // print_r($pages);
