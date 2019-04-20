edac4da3727822dbe2a30bf1efa9763c8599a7a1
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2019-04-19 21:23:35 -0300

    Autenticação

--- application/hooks/AuthHook.php
+++ application/hooks/AuthHook.php
@@ -2,8 +2,26 @@
 
 class AuthHook
 {
+    private $controller = [
+        'auth'
+    ];
+
     public function check()
     {
-        echo 'AuthHook::check';
+        $CI =& get_instance();
+
+        if(!isset($CI->session)) {
+            $CI->load->library('session');
+        }
+
+        $CI->load->helper('url');
+
+        $user = $CI->session->user ?? null;     
+        $controller = $CI->uri->segment(1);     //Segment pega o primeiro item da url, no caso 'auth'  http://localhost:8000/auth/login
+
+        if (!$user and !in_array($controller, $this->controller)) {
+            redirect('auth/login', 'locations', 302);
+            die();
+        }
     }
 }
